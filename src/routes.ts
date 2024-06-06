0fdf3ae6d9e3eeaa28dbfd55a3b8d2342d2a0ac0
danielrl98 <daniel-rl-98@hotmail.com>
2024-06-06 08:39:31 -0300

    first

---
+++ src/routes.ts
@@ -0,0 +1,21 @@
+
+import env from './shared/global/env'
+import express from 'express'
+import RequestWebhookController from './features/webhook/request/request.controller'
+const router = express.Router()
+
+router.post('/request', async (req, res, next) => {
+    const body = req.body
+
+    const result = await new RequestWebhookController().create(JSON.stringify(body))
+
+    res.send(result)
+
+    next()
+})
+
+router .get('/response', async (req, res, next) => {
+    res.render('dashboard/teste', {teste: {teste: 1000}})
+})
+
+export default router
--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ require('dotenv').config()
 import env from './shared/global/env'
 import express from 'express'
 import cors from 'cors'
-import RequestWebhookController from './features/webhook/request/request.controller'
 import hbs from './shared/config/hbs'
+import router from './routes'
 
 const app = express()
 
@@ -14,20 +14,8 @@ app.use(express.json())
 app.engine('.hbs',hbs.engine)
 app.set('view engine','.hbs')
 app.set("views", __dirname + "/views/")
+app.use('/webhook',router)
 
-app.post('/webhook/request', async (req, res, next) => {
-    const body = req.body
-
-    const result = await new RequestWebhookController().create(JSON.stringify(body))
-
-    res.send(result)
-
-    next()
-})
-
-app.get('/webhook/response', async (req, res, next) => {
-    res.render('dashboard/teste', {teste: {teste: 1000}})
-})
 
 const PORT = 8000
 
