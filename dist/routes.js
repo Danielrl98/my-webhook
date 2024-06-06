eea0bbc24c86a57fd3364a1234a238692678e0ce
danielrl98 <daniel-rl-98@hotmail.com>
2024-06-06 13:13:26 -0300

    ajustes finais

--- dist/routes.js
+++ dist/routes.js
@@ -4,16 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const request_controller_1 = __importDefault(require("./features/webhook/request/request.controller"));
 const router = express_1.default.Router();
-/*
 router.post('/request', async (req, res, next) => {
-    const body = req.body
-    const result = await new RequestWebhookController().create(JSON.stringify(body))
-
-    res.send(result)
-
-    next()
-}) */
+    const body = req.body;
+    const result = await new request_controller_1.default().create(JSON.stringify(body));
+    res.send(result);
+    next();
+});
 router.get('/', async (req, res, next) => {
     res.send('hello world3');
 });
