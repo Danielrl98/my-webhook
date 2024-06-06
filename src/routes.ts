6256ed13bf5e70f398ada3bad54dcefb450cfa42
danielrl98 <daniel-rl-98@hotmail.com>
2024-06-06 12:11:58 -0300

    ajs

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import RequestWebhookController from './features/webhook/request/request.controller'
 const router = express.Router()
-
+/*
 router.post('/request', async (req, res, next) => {
     const body = req.body
     const result = await new RequestWebhookController().create(JSON.stringify(body))
@@ -9,7 +9,7 @@ router.post('/request', async (req, res, next) => {
     res.send(result)
 
     next()
-}) 
+}) */
 
 router.get('/', async (req, res, next) => {
     res.send('hello world2')
